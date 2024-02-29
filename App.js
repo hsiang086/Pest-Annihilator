e6a20d4e13d800e2eff100b3add5a7bd0ef224f8
hsiang086 <fabiowu20070806@gmail.com>
2024-02-29 23:30:07 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,66 @@
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
+
+import axios from 'axios';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/pest.jpg');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  const detectPestApi = async () => {
+    const base64 = await FileSystem.readAsStringAsync(fileUri=selectedImage, { encoding: 'base64' });
+    axios({
+      method: 'POST',
+      url: 'https://detect.roboflow.com/pest-detection-qvpee/3',
+      params: {
+        api_key: 'ToumOw2ArLdsofKlQMYQ',
+      },
+      data: base64,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+    .then(function (response) {
+      console.log(response.data['predictions']);
+    })
+    .catch(function (error) {
+      console.log(error.response.data);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button theme="secondary" label="Detect Pest" onPress={detectPestApi} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +69,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex:1, 
+    paddingTop: 58
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center">
+    <img src="./Github/Pest-Annihilator.webp" style="border-radius: 25px; width: 60%"></img>
+</p>
+
+<h1 align="center" style="font-weight: bold;">Pest-Annihilator No.1588</h1>
+
+<p align="center">
+  <img alt="Version" src="https://img.shields.io/badge/version-0.0.1-success.svg?cacheSeconds=2592000" />
+  <img alt="Version" src="https://img.shields.io/badge/license-MIT-blue.svg?cacheSeconds=2592000" />
+  <img alt="Version" src="https://img.shields.io/badge/made%20with-react--native-blue.svg?cacheSeconds=2592000" />
+</p>
+
+## Description
+
+- A mobile app that detect pests and destroy them.
+- This app is powered by expo and react-native.
+
+## Features
+
+- Detect pests using [roboflow's](https://universe.roboflow.com/) api.
+- Destroy pests, we will (probably) made a custom arduino based robot to destroy pests soon.
+- comming soon...
+
+## Installation
+
+```bash
+npm i
+npx expo start
+```
+
+
